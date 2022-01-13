bfebebc788f228074da5179dadcda1bd220e711d
Adriaan P Hilbers <a.hilbers17@imperial.ac.uk>
2022-01-13 19:27:08 +0000

    render one block moving across map

--- main.cpp
+++ main.cpp
@@ -2,16 +2,94 @@
 #include <unistd.h>
 #include <vector>
 
-int main() {
-  float pause = 2.;
 
-  for (int i=0; i<10; i++) {
-    printf("\x1b[2J");  // clear screen
-    printf("\x1b[H"); // move cursor to top
-    sleep(pause);
-    printf("%d\n", i);
+void render_map(int map_side_length, std::string map_string) {
+
+  // To make map appear more square, each grid square is 2x1 characters
+  int row_char_length = 2 * map_side_length;
+  int num_rows = map_side_length;
+  std::string block = "\u2588\u2588";
+
+  // Duplicate each character in 'map_string' to make it work with 2x1 grid
+  std::string render_map_string(row_char_length * num_rows, ' ');
+  for (int i=0; i<row_char_length*num_rows; i++){
+    render_map_string[2*i] = map_string[i];
+    render_map_string[2*i+1] = map_string[i];
+  }
+
+  // Render a horizontal edge, used at top and bottom of map
+  auto render_horizontal_edge = [](int map_side_length, std::string block) {
+    for (int i=0; i<map_side_length+2; i++) std::cout << block;
+    std::cout << std::endl;
   };
-  return 0;
+
+  // Render the map by printing the string in rows of length 'row_length'
+  std::string map_string_row(row_char_length, ' ');
+  render_horizontal_edge(map_side_length, block);
+  int pos_of_x;
+  for (int row_num=0; row_num<num_rows; row_num++) {
+    map_string_row = {
+      render_map_string.substr(row_num * row_char_length, row_char_length)
+    };
+    pos_of_x = map_string_row.find('X');
+    if (pos_of_x != -1) {
+      map_string_row.replace(pos_of_x+1, 1, "\u2588");
+      map_string_row.replace(pos_of_x, 1, "\u2588");
+    }  
+    std::cout << block << map_string_row << block << std::endl;
+  }
+  render_horizontal_edge(map_side_length, block);
+  std::cout << std::endl;
+}
+
+
+int main() {
+
+  // std::string filled_square = "\u2588";  // filled rectangle
+
+  int pause = 100000;  // pause in microseconds
+  int map_side_length = 8;
+  std::string filled_square = "X";
+  std::string map_string(map_side_length * map_side_length, ' ');
+
+  int num_rows = map_side_length;
+  int row_length = map_side_length;
+  std::string map_string_row;
+
+  
+
+  for (int step=0; step<map_string.length(); step++) {
+
+    std::cout << "\x1b[2J\x1b[H";  // clear screen and move cursor to top
+
+    map_string.replace(step, 1, filled_square);
+    render_map(map_side_length, map_string);
+    map_string.replace(step, 1, " ");
+    usleep(pause);
+  }
+
+
+
+  
+  
+  
+  // std::string map_string_row;
+  // std::cout << '+' << horizontal_edge << '+' << std::endl;
+  // for (int row_num=0; row_num<map_side_length; row_num++) {
+  //   map_string_row = map_string_repeated.substr(row_num * map_side_length, 2*map_side_length);
+  //   std::cout << '|' << map_string_row << '|' << std::endl;
+  // }
+  // std::cout << '+' << horizontal_edge << '+' << std::endl;
+
+
+
+
+  // for (int i=0; i<10; i++) {
+  
+  //   sleep(pause);
+  //   printf("%d\n", i);
+  // };
+  // return 0;
 }
 
 
