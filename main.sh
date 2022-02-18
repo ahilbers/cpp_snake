4423b906592dcfece522c2fa27871b2ca96bb2cc
Adriaan P Hilbers <a.hilbers17@imperial.ac.uk>
2022-02-18 15:01:08 +0000

    move objects to objects.cpp, ready to move to header

--- main.sh
+++ main.sh
@@ -1,2 +1,5 @@
-clang++ --std=c++11 --stdlib=libc++ --output=main.out main.cpp utils.cpp
+clang++ --std=c++11 \
+        --stdlib=libc++ \
+        --output=main.out \
+        utils.cpp objects.cpp main.cpp
 # ./main.out
