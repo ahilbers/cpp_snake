c89ce6d54d42b58a4897f085d426f295c00d09bd
Adriaan P Hilbers <a.hilbers17@imperial.ac.uk>
2022-02-19 22:54:03 +0000

    add new functions to game.h

--- game.h
+++ game.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include "objects.h"
+
 namespace game {
+  void start_game(objects::Snake snake, objects::Egg egg, objects::Map map);
+  void display_status(int map_side_length, int num_steps, int score);
   void exit_game();
   void read_user_input(int* user_input);
   void play_game();
