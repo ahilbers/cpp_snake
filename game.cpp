f005761ede26503c4e47dbc1728073b0e9ee76d7
Adriaan P Hilbers <a.hilbers17@imperial.ac.uk>
2022-02-19 22:19:10 +0000

    clean up includes

--- game.cpp
+++ game.cpp
@@ -1,9 +1,4 @@
-#include <iostream>
 #include <unistd.h>
-#include <stdio.h>
-#include <time.h>
-#include <vector>
-#include <deque>
 #include <thread>
 #include "game.h"  // exit_game, read_user_input, play_game
 #include "objects.h"  // Snake, Egg, Map
